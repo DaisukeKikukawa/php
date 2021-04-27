775f0ec4214271152967ddda9d3dd7e7f1a33772
Daisuke <daisuke.kikukawa.16@gamail.com>
2021-04-27 21:40:09 +0900

    不要なスペースの削除

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,11 +3,11 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-  for ($n=1; $n < count($array) ; $n++) { 
-    if($array[$n-1] > $array[$n]) {
+  for ($n = 1; $n < count($array); $n++) {
+    if ($array[$n - 1] > $array[$n]) {
       $temp = $array[$n];
-      $array[$n] = $array[$n-1];
-      $array[$n-1] = $temp;
+      $array[$n] = $array[$n - 1];
+      $array[$n - 1] = $temp;
     }
   }
 }
