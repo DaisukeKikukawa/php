5236f7ab3a4a564918ba21177de74e12d82c8e2c
Daisuke <daisuke.kikukawa.16@gamail.com>
2021-04-27 21:01:50 +0900

    練習問題2番になります。 確認のほど宜しくお願いいたします。

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,7 +3,13 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-
+  for ($n=1; $n < count($array) ; $n++) { 
+    if($array[$n-1] > $array[$n]) {
+      $temp = $array[$n];
+      $array[$n] = $array[$n-1];
+      $array[$n-1] = $temp;
+    }
+  }
 }
 // 修正はここまで
 
