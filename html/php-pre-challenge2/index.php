1910492334b2e39d580ec6fb42814ae5e263c74c
Daisuke <daisuke.kikukawa.16@gamail.com>
2021-04-28 20:03:39 +0900

    変数名の変更 関数の置き換え

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -1,13 +1,15 @@
 <?php
 $array = explode(',', $_GET['array']);
 
+$array_number = count($array);
+
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-  for ($n = 1; $n < count($array); $n++) {
-    if ($array[$n - 1] > $array[$n]) {
-      $temp = $array[$n];
-      $array[$n] = $array[$n - 1];
-      $array[$n - 1] = $temp;
+for ($i = 0; $i < $array_number; $i++) {
+  for ($j = 1; $j < $array_number; $j++) {
+    if ($array[$j - 1] > $array[$j]) {
+      $temp = $array[$j];
+      $array[$j] = $array[$j - 1];
+      $array[$j - 1] = $temp;
     }
   }
 }
