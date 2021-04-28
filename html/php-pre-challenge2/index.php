5abb064ed18d1a830fa5ed510cdd0715aee793a3
Daisuke <daisuke.kikukawa.16@gamail.com>
2021-04-28 20:53:40 +0900

    変数名の変更を行いました。

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -1,11 +1,11 @@
 <?php
 $array = explode(',', $_GET['array']);
 
-$array_number = count($array);
+$array_length = count($array);
 
 // 修正はここから
-for ($i = 0; $i < $array_number; $i++) {
-  for ($j = 1; $j < $array_number; $j++) {
+for ($i = 0; $i < $array_length-1; $i++) {
+  for ($j = 1; $j < $array_length-$i; $j++) {
     if ($array[$j - 1] > $array[$j]) {
       $temp = $array[$j];
       $array[$j] = $array[$j - 1];
